df15588797e52a86e590268fb4a4195db39c3b4b
Jakub Olszak <olsza.op@gmail.com>
2023-08-26 11:19:03 +0200

    added test_review and test_search

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+from pytest import fixture
+
+
+@fixture(name="beer_name")
+def fixture_beer_name():
+    return "test_beer_name"
+
+
+@fixture(name="alcohol_amout")
+def fixture_alcohol_amount():
+    return 9999.0
+
+
+@fixture(name="review_id")
+def fixture_review_id():
+    return 1
+
+
+@fixture(name="rating_value")
+def fixture_rating_value():
+    return 999.0
+
+
+@fixture(name="description")
+def fixture_description():
+    return "test_description"
+
+
+@fixture(name="added_date")
+def fixture_added_date():
+    return "test_added_date"
+
+
+@fixture(name="limit")
+def fixture_limit():
+    return 9999
---
+++ tests/test_review.py
@@ -0,0 +1,151 @@
+from fastapi.testclient import TestClient
+from unittest import mock
+from http import HTTPStatus
+from main import app
+from unittest import mock
+from tests.conftest import (
+    fixture_beer_name,
+    fixture_alcohol_amount,
+    fixture_review_id,
+    fixture_rating_value,
+    fixture_description,
+    fixture_added_date,
+    fixture_limit
+)
+
+client = TestClient(app)
+
+
+def test_add_review(beer_name, description, alcohol_amout, rating_value, review_id):
+    response_data = {"id": review_id,
+                     "name": beer_name,
+                     "description": description,
+                     "alcohol": alcohol_amout,
+                     "rating": rating_value}
+
+    with mock.patch("databases.Database.execute", return_value=review_id):
+        params = {
+            "name": beer_name,
+            "description": description,
+            "alcohol": alcohol_amout,
+            "rating": rating_value
+        }
+        response = client.post("/review/add", params=params)
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == response_data
+
+
+def test_get_reviews_by_rating(review_id, beer_name, rating_value, alcohol_amout,
+                               description, added_date, limit):
+
+    expected_data = {
+        "id": review_id,
+        "name": beer_name,
+        "rating": rating_value,
+        "alcohol": alcohol_amout,
+        "description": description,
+        "added": added_date
+    }
+
+    with mock.patch("databases.Database.fetch_all", return_value=expected_data):
+        params = {
+            "limit": limit,
+            "sort_by": "rating"
+        }
+        response = client.get("/review/get/all/", params=params)
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == expected_data
+
+
+def test_get_reviews_by_alcohol(review_id, beer_name, rating_value, alcohol_amout,
+                                description, added_date, limit):
+
+    expected_data = {
+        "id": review_id,
+        "name": beer_name,
+        "rating": rating_value,
+        "alcohol": alcohol_amout,
+        "description": description,
+        "added": added_date
+    }
+
+    with mock.patch("databases.Database.fetch_all", return_value=expected_data):
+        params = {
+            "limit": limit,
+            "sort_by": "alcohol"
+        }
+        response = client.get("/review/get/all/", params=params)
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == expected_data
+
+
+def test_get_reviews_by_name(review_id, beer_name, rating_value, alcohol_amout,
+                             description, added_date, limit):
+
+    expected_data = {
+        "id": review_id,
+        "name": beer_name,
+        "rating": rating_value,
+        "alcohol": alcohol_amout,
+        "description": description,
+        "added": added_date
+    }
+
+    with mock.patch("databases.Database.fetch_all", return_value=expected_data):
+        params = {
+            "limit": limit,
+            "sort_by": "name"
+        }
+        response = client.get("/review/get/all/", params=params)
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == expected_data
+
+
+def test_get_review_by_id(review_id, beer_name, rating_value, alcohol_amout,
+                          description, added_date):
+
+    expected_data = {
+        "id": review_id,
+        "name": beer_name,
+        "rating": rating_value,
+        "alcohol": alcohol_amout,
+        "description": description,
+        "added": added_date
+    }
+
+    with mock.patch("databases.Database.fetch_one", return_value=expected_data):
+        response = client.get(f"/review/get/{review_id}")
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == expected_data
+
+
+def test_get_review_by_name(review_id, beer_name, rating_value, alcohol_amout,
+                            description, added_date):
+
+    expected_data = {
+        "id": review_id,
+        "name": beer_name,
+        "rating": rating_value,
+        "alcohol": alcohol_amout,
+        "description": description,
+        "added": added_date
+    }
+
+    with mock.patch("databases.Database.fetch_all", return_value=expected_data):
+        response = client.get(f"/review/get/{beer_name}/")
+
+        assert response.status_code == HTTPStatus.OK
+        assert response.json() == expected_data
+
+
+def test_update_review():
+    ...
+
+
+def test_delete_review():
+    ...

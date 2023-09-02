96aa0d35b3b8050b53209e676f1356ed1d98e701
Jakub Olszak <olsza.op@gmail.com>
2023-09-02 15:17:14 +0200

    feat: Implemented exceptions in Search category

--- app/api/search.py
+++ app/api/search.py
@@ -3,8 +3,9 @@ from sqlalchemy import select
 from app.models.review import Review
 from app.schemas.review import DefaultResponse
 from app.db.database import database
-from app.exceptions.exceptions import NotFoundException
+from app.exceptions.exceptions import NotFoundException, BadValue
 from typing import List
+from app.config import settings
 
 router = APIRouter()
 
@@ -12,6 +13,11 @@ router = APIRouter()
 @router.get("/by-alcohol", response_model=List[DefaultResponse], summary="Search reviews by alcohol content",
             description="Returns a list of reviews that have the specified alcohol content.")
 async def search_by_alcohol(alcohol: float):
+    if alcohol > settings.ALCOHOL_LIMIT or alcohol < 0:
+        raise BadValue("The specified alcohol content is not valid. Please provide an alcohol content between 0.0 and 20.0.")
+    elif alcohol % 0.1 != 0:
+        raise BadValue("The provided alcohol amount should have at most one decimal place.")
+    
     query = select(Review).where(Review.alcohol == alcohol)
     response = await database.fetch_all(query)
 
@@ -25,6 +31,11 @@ async def search_by_alcohol(alcohol: float):
 @router.get("/by-rating", response_model=List[DefaultResponse], summary="Search reviews by rating",
             description="Returns a list of reviews that have the specified rating.")
 async def search_by_rating(rating: float):
+    if rating > settings.RATING_RANGE or rating < 0:
+        raise BadValue("The provided rating is not valid. Please enter a rating between 0 and 10.0.")
+    elif rating % 0.1 != 0:
+        raise BadValue("The provided rating should have at most one decimal place.")
+    
     query = select(Review).where(Review.rating == rating)
     response = await database.fetch_all(query)
 
@@ -37,8 +48,10 @@ async def search_by_rating(rating: float):
 @router.get("/by-desc", response_model=List[DefaultResponse], summary="Search reviews by description",
             description="Returns a list of reviews that have the specified keyword in their description.")
 async def search_by_desc(desc: str):
+    if len(desc) > settings.DESC_MAX_LEN or len(desc) < settings.DESC_MIN_LEN:
+        raise BadValue("The provided review description is not valid. Please enter a description between 5 and 250 characters in length.")
+    
     query = select(Review).where(Review.description.ilike(f"%{desc}"))
-
     response = await database.fetch_all(query)
 
     if not response:
--- app/config.py
+++ app/config.py
@@ -3,5 +3,9 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./database.db"
     REVIEW_LIMIT: int = 30
+    ALCOHOL_LIMIT: float = 20.0
+    RATING_RANGE: float = 10.0
+    DESC_MIN_LEN: int = 5
+    DESC_MAX_LEN: int = 250
 
 settings = Settings()

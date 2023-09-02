b61bab0b02a9448dffb79d1e6f9766a6b03a2e30
Jakub Olszak <olsza.op@gmail.com>
2023-09-02 15:53:41 +0200

    fix: Fix mypy errors

--- app/utils/util.py
+++ app/utils/util.py
@@ -2,10 +2,7 @@ def has_one_decimal_place(rating: float) -> bool:
     rating_str = str(rating)
     parts = rating_str.split('.')
     
-    if len(parts[1]) == 1 or len(parts[1] == 0):
+    if len(parts) == 2 and len(parts[1]) == 1:
         return True
     else:
         return False
-
-
-print(has_one_decimal_place(5.0))

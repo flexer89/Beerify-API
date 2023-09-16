2664fc75dad0016fe886c604e4f59e505a5bca32
Jakub Olszak <olsza.op@gmail.com>
2023-09-16 23:26:07 +0200

    fix: fix unittest script

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python -m pip install --no-cache-dir --upgrade pip
-pip install --no-cache-dir -r requirements.txt
+python3 -m pip install --no-cache-dir --upgrade pip
+python3 pip install --no-cache-dir -r requirements.txt
 
-python -m pytest tests/
+python3 -m pytest tests/
 find . -name "*.py" | xargs mypy
